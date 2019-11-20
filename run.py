4af585d22019959acb2aed51e376ce6b1cf3a554
faithforus <ljunf817@163.com>
2019-11-20 14:48:53 +0800

    'build'

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def get_netcard():
 interface, local_ip = get_netcard()[0]
 
 if __name__ == '__main__':
-    print("start at:",local_ip)
-    # io.run(app, host="0.0.0.0")
-    io.run(app, host=local_ip)
+    print("start at:", local_ip)
+    io.run(app, host="0.0.0.0")
+    # io.run(app, host=local_ip)
     # webbrowser.open("http://127.0.0.1:5000/login")
